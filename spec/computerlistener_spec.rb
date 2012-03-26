410c7e6ca602753deaf66fe677c4c2e14bda5f5b
Jens Christensen <0xchristensen@gmail.com>
2012-03-26 15:15:42 +0200

    fixing tests to work with new mailing function

--- spec/computerlistener_spec.rb
+++ spec/computerlistener_spec.rb
@@ -1,17 +1,8 @@
 $LOAD_PATH.push(File.dirname(__FILE__) + "/../models/src")
 
-def import(path)
-end
-
 module Jenkins
   module Slaves
     module ComputerListener
-      def online(computer, listener)
-        puts computer
-      end
-      def offline(computer)
-        puts computer
-      end
     end
   end
 end
@@ -19,9 +10,11 @@ end
 require 'environmentvariablesnodeproperty'
 
 module Java_Wrapper
-  def getSmtpValues
-    ["smtpServer", "smtpPort", "adminAddress"]
-  end
+
+  @@numberOfEmailsSent = 0
+  @@arrayOfEmails = []
+  @@mail = ""
+  
   def getEnvVariableArray(envVar)
     for nodeProp in Jenkins.getInstance().getGlobalNodeProperties()
       if nodeProp.is_a? EnvironmentVariablesNodeProperty
@@ -30,39 +23,54 @@ module Java_Wrapper
       end
     end
   end
+  
+  def sendEmail(mail, name, online)
+    @@numberOfEmailsSent += 1
+    @@arrayOfEmails.push(mail)
+    @@mail = mail
+  end
+  
+  def getNumberOfEmailsSent()
+    return @@numberOfEmailsSent
+  end
+  
+  def restoreState()
+    @@numberOfEmailsSent = 0
+    @@arrayOfEmails = []
+    @@mail = ""
+  end
+  
+  def expandedVariables array
+    if @@arrayOfEmails == array
+      return "expanded correctly"
+    else
+      return "something went wrong"
+    end
+  end
+  
+  def self.expandedAddressField
+    if @@mail == ""
+      return nil
+    else
+      return @@mail
+    end
+  end
+  
   class Mailer
-    smtpServer = ""
-    smtpPort = ""
-    adminAddress = ""
     def self.descriptor
       self
     end
-    def self.getSmtpServer
-      smtpServer
-    end
-    def self.getSmtpPort
-      smtpPort
-    end
-    def self.getAdminAddress
-      adminAddress
-    end
-    def self.setSmtpServer server
-      smtpServer = server
-    end
-    def self.setSmtpPort port
-      smtpPort = port
-    end
-    def self.setAdminAddress admin
-      adminAddress = admin
-    end
   end
+  
   class Jenkins
     def self.getInstance
       self
     end
+    
     def self.setEnvHash(envVarsHash)
       @instEnvVarsHashMap = envVarsHash      
     end
+    
     def self.getGlobalNodeProperties
       return [EnvironmentVariablesNodeProperty.new(@instEnvVarsHashMap)]
     end
@@ -74,61 +82,49 @@ include Java_Wrapper
 
 require 'emailcomputerlistener'
 require 'computer'
-require 'smtp'
 
 describe EmailComputerListener do
   before :each do
     @ecl = EmailComputerListener.new
-    Net::SMTP::restoreState
+    Java_Wrapper::restoreState()
     Java_Wrapper::Jenkins::setEnvHash({})
   end
 
   it "should send an email if node is configured" do
     EmailNodeProperty.setAddressField "name@example.com"
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::emailSent?.should == true
+    Java_Wrapper::getNumberOfEmailsSent.should eq(1)
   end
 
   it "should not send email if node is unconfigured" do
     EmailNodeProperty.setAddressField "name@example.com"
     @ecl.offline(NotConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::emailSent?.should == false
-  end
-
-  it "should have values for smtp, port, return address and to address" do
-    EmailNodeProperty.setAddressField "name@example.com"
-    @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::areValuesSet?.should == true
-  end
-  
-  it "should not have the correct values if node is not configured" do
-    @ecl.offline(NotConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::areValuesSet?.should == false
+    Java_Wrapper::getNumberOfEmailsSent.should eq(0)
   end
   
   it "should send one email per email address in the node configurations" do
     EmailNodeProperty.setAddressField "name@example.com name2@example.com name3@example.com"
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(3)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(3)
   end
   
   it "should only send one email if only one email is in the list" do
     EmailNodeProperty.setAddressField "name@example.com"
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(1)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(1)
   end
   
   it "should not send any emails if it's the master going down (offlineCause == nil)" do
     EmailNodeProperty.setAddressField "name@example.com"
     @ecl.offline(ConfiguredComputerOfflineCauseNull.new("ThisIsASlave"))
-    Net::SMTP::emailSent?.should == false
+    Java_Wrapper::getNumberOfEmailsSent.should eq(0)
   end
   
   it "should send an online message if it went offline earlier" do
     EmailNodeProperty.setAddressField "name@example.com"
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
     @ecl.online(ConfiguredComputer.new("ThisIsASlave"), "Does not matter")
-    Net::SMTP::numberOfEmailsSent.should eq(2)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(2)
   end
   
   it "should expand a single environment variable" do
@@ -136,13 +132,13 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR"=>"name@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::expandedAddressField.should eq("name@example.com")
+    Java_Wrapper::expandedAddressField.should eq("name@example.com")
   end
   
   it "should return nil if environment variable does not exist" do
     EmailNodeProperty.setAddressField "$ENVVAR"
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::expandedAddressField.should eq nil
+    Java_Wrapper::expandedAddressField.should eq nil
   end
   
   it "should expand a single environment variable with several emails in it" do
@@ -150,9 +146,9 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR"=>"name1@example.com name2@example.com name3@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(3)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(3)
     array = ["name1@example.com", "name2@example.com" ,"name3@example.com"]
-    Net::SMTP::expandedVariables(array).should eq("expanded correctly")
+    Java_Wrapper::expandedVariables(array).should eq("expanded correctly")
   end
   
   it "should expand several environment variables" do
@@ -160,9 +156,9 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR1"=>"name1@example.com", "ENVVAR2"=>"name2@example.com", "ENVVAR3"=>"name3@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(3)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(3)
     array = ["name1@example.com", "name2@example.com" ,"name3@example.com"]
-    Net::SMTP::expandedVariables(array).should eq("expanded correctly")
+    Java_Wrapper::expandedVariables(array).should eq("expanded correctly")
   end
   
   it "should take a mix between environment variables and emails" do
@@ -170,9 +166,9 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR1"=>"name1@example.com", "ENVVAR2"=>"name3@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(3)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(3)
     array = ["name1@example.com", "name2@example.com" ,"name3@example.com"]
-    Net::SMTP::expandedVariables(array).should eq("expanded correctly")
+    Java_Wrapper::expandedVariables(array).should eq("expanded correctly")
   end
   
   it "should handle a non-existent environment variable in a string with several other entries" do
@@ -180,9 +176,9 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR2"=>"name3@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(2)
+    Java_Wrapper::getNumberOfEmailsSent.should eq(2)
     array = ["name2@example.com" ,"name3@example.com"]
-    Net::SMTP::expandedVariables(array).should eq("expanded correctly")
+    Java_Wrapper::expandedVariables(array).should eq("expanded correctly")
   end
   
   it "should handle several environment variables with several emails in each" do
@@ -190,8 +186,8 @@ describe EmailComputerListener do
     hashMap = {"ENVVAR1"=>"name1@example.com name2@example.com", "ENVVAR2"=>"name3@example.com name4@example.com"}
     Java_Wrapper::Jenkins::setEnvHash(hashMap)
     @ecl.offline(ConfiguredComputer.new("ThisIsASlave"))
-    Net::SMTP::numberOfEmailsSent.should eq(4)
-    array = ["name1@example.com", "name2@example.com" ,"name3@example.com", "name4@example.com"]
-    Net::SMTP::expandedVariables(array).should eq("expanded correctly")
+    Java_Wrapper::getNumberOfEmailsSent.should eq(4)
+    array = ["name1@example.com", "name2@example.com", "name3@example.com", "name4@example.com"]
+    Java_Wrapper::expandedVariables(array).should eq("expanded correctly")
   end
 end
